acfa160b677700b91de934b434cab1a5f37aa478
Marc Ermshaus <marc@ermshaus.org>
2012-03-21 18:25:15 +0100

    Fixed a hardcoded path in Inigo tests. Started to add more tests to Inigo. Minor changes.

--- src/Kaloa/Renderer/Inigo/Handler/ImgHandler.php
+++ src/Kaloa/Renderer/Inigo/Handler/ImgHandler.php
@@ -5,25 +5,43 @@ namespace Kaloa\Renderer\Inigo\Handler;
 use Kaloa\Renderer\Inigo\Handler\ProtoHandler;
 use Kaloa\Renderer\Inigo\Parser;
 
+/**
+ *
+ */
 class ImgHandler extends ProtoHandler
 {
+    /**
+     *
+     */
     public function __construct()
     {
-        $this->name = "img";
+        $this->name = 'img';
         $this->type = Parser::TAG_OUTLINE;
     }
 
-    private function DrawImage($path, $alt = "", $title = "",
-            $align = Parser::PC_IMG_ALIGN_LEFT, $dir = "")
+    /**
+     *
+     * @param  string $path
+     * @param  string $alt
+     * @param  string $title
+     * @param  int    $align
+     * @param  string $dir
+     * @return string
+     */
+    protected function DrawImage($path, $alt = '', $title = '',
+            $align = Parser::PC_IMG_ALIGN_LEFT, $dir = '')
     {
-        $border_width = 1; // width of framing div's border (one side)
+        // width of framing div's border (one side)
+        $border_width = 1;
+
         $padding      = 2;
-        $ret          = "";
+        $ret          = '';
 
         $path         = $dir . $path;
 
         //$title = $this->ParseInline($title);
 
+        // Image exists?
         if (getimagesize($path) === false) {
             return;
         }
@@ -32,85 +50,94 @@ class ImgHandler extends ProtoHandler
 
         //$path = $this->m_rel_path . $path;
 
-        if ($alt == "") {
+        if ($alt === '') {
             $alt = $path;
         }
 
-        $style = "width: " . ($width + 2 * (1 + $border_width + $padding)) . "px;";
+        $style = 'width: ' . ($width + 2 * (1 + $border_width + $padding)) . 'px;';
         //$style2 .= "height: " . ($height + 2 * ($border_width )) . "px;";
 
         if ($align == Parser::PC_IMG_ALIGN_RIGHT) {
-            $ret .= "<div class=\"img\">\n";
-            $ret .= "  <div class=\"img_right\" style=\"$style\">\n";
-            $ret .= "    <div class=\"img_border\">\n";
-            $ret .= "      <img src=\"$path\" alt=\"Bild: $alt\" title=\"$alt\" $attr />\n";
-            $ret .= "    </div>\n";
-            $ret .= "    <div class=\"img_content\">";
+            $ret .= '<div class="img">' . "\n"
+                  . '  <div class="img_right" style="' . $style . '">' . "\n"
+                  . '    <div class="img_border">' . "\n"
+                  . '      <img src="' . $path . '" alt="Bild: ' . $alt . '" title="'
+                      . $alt . '" ' . $attr . ' />' . "\n"
+                  . '    </div>' . "\n"
+                  . '    <div class="img_content">';
         } else if ($align == Parser::PC_IMG_ALIGN_CENTER) {
-            $ret .= "<div class=\"center\">\n";
-            $ret .= "  <div class=\"img\">\n";
-            $ret .= "    <div class=\"img_center\" style=\"$style\">\n";
-            $ret .= "      <div class=\"img_border\">\n";
-            $ret .= "        <img src=\"$path\" alt=\"Bild: $alt\" title=\"$alt\" $attr />\n";
-            $ret .= "      </div>\n";
-            $ret .= "      <div class=\"img_content\">";
+            $ret .= '<div class="center">' . "\n"
+                  . '  <div class="img">' . "\n"
+                  . '    <div class="img_center" style="' . $style . '">' . "\n"
+                  . '      <div class="img_border">' . "\n"
+                  . '        <img src="' . $path . '" alt="Bild: ' . $alt . '" title="'
+                      . $alt . '" ' . $attr . ' />' . "\n"
+                  . '      </div>' . "\n"
+                  . '      <div class="img_content">';
         } else if ($align == Parser::PC_IMG_ALIGN_LEFT) {
-            $ret .= "<div class=\"img\">\n";
-            $ret .= "  <div class=\"img_left\" style=\"$style\">\n";
-            $ret .= "    <div class=\"img_border\">\n";
-            $ret .= "      <img src=\"$path\" alt=\"Bild: $alt\" title=\"$alt\" $attr />\n";
-            $ret .= "    </div>\n";
-            $ret .= "    <div class=\"img_content\">";
+            $ret .= '<div class="img">' . "\n"
+                  . '  <div class="img_left" style="' . $style . '">' . "\n"
+                  . '    <div class="img_border">' . "\n"
+                  . '      <img src="' . $path . '" alt="Bild: ' . $alt . '" title="'
+                      . $alt . '" ' . $attr . ' />' . "\n"
+                  . '    </div>' . "\n"
+                  . '    <div class="img_content">';
         }
 
         return $ret;
     }
 
+    /**
+     *
+     * @param  array  $data
+     * @return string
+     */
     public function draw(array $data)
     {
-        if ($data["front"]) {
-            $src = "";
-            $title = "";
+        $ret = '';
+
+        if ($data['front']) {
+            $src   = '';
+            $title = '';
             $align = null;
-            if (isset($data["params"]["(default)"])) {
-                $src = $data["params"]["(default)"];
-            } elseif (isset($data["params"]["src"])) {
-                $src = $data["params"]["src"];
+
+            if (isset($data['params']['(default)'])) {
+                $src = $data['params']['(default)'];
+            } else if (isset($data['params']['src'])) {
+                $src = $data['params']['src'];
             }
 
-            if (isset($data["params"]["title"])) {
-                $title = $data["params"]["title"];
+            if (isset($data['params']['title'])) {
+                $title = $data['params']['title'];
             }
 
-            if (isset($data["params"]["align"])) {
-                $align = $data["params"]["align"];
+            if (isset($data['params']['align'])) {
+                $align = $data['params']['align'];
             }
 
             //$src = $this->m_image_path . $src;
             $this->last_img_align = $align;
 
             switch ($align) {
-                case "right":
+                case 'right':
                     $align = Parser::PC_IMG_ALIGN_RIGHT;
                     break;
-
-                case "center":
+                case 'center':
                     $align = Parser::PC_IMG_ALIGN_CENTER;
                     break;
-
                 default:
                     $align = Parser::PC_IMG_ALIGN_LEFT;
                     break;
             }
 
-            return $this->DrawImage($src, $title, "", $align, $data["vars"]["image-dir"]);
+            $ret = $this->DrawImage($src, $title, '', $align, $data['vars']['image-dir']);
+        } else if ($this->last_img_align === 'center') {
+            $this->last_img_align = '';
+            $ret = "</div>\n    </div>\n  </div>\n</div>\n\n";
         } else {
-            if ($this->last_img_align == "center") {
-                $this->last_img_align = "";
-                return "</div>\n    </div>\n  </div>\n</div>\n\n";
-            } else {
-                return "</div>\n  </div>\n</div>\n\n";
-            }
+            $ret = "</div>\n  </div>\n</div>\n\n";
         }
+
+        return $ret;
     }
 }
--- tests/Kaloa/Tests/Renderer/InigoRendererTest.php
+++ tests/Kaloa/Tests/Renderer/InigoRendererTest.php
@@ -10,13 +10,19 @@ use Kaloa\Renderer\Inigo\Parser;
 use Kaloa\Renderer\Inigo\Handler\AbbrHandler;
 use Kaloa\Renderer\Inigo\Handler\FootnotesHandler;
 
+/**
+ *
+ */
 class InigoRendererTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     *
+     */
     public function testIntegrity()
     {
         // Environment
         $contentToRender = file_get_contents(__DIR__ . '/examples/inigo/klangbilder.txt');
-        $resourceBasePath = __DIR__ . '/examples/inigo/klangbilder';
+        $resourceBasePath = __DIR__ . '/examples/inigo/res';
         $filter = 'inigo';
 
         $config = new Config();
@@ -31,9 +37,14 @@ class InigoRendererTest extends PHPUnit_Framework_TestCase
 
         $expected = file_get_contents(__DIR__ . '/examples/inigo/klangbilder.expected');
 
+        $expected = str_replace('__RESOURCE_BASE_PATH__', $config->getResourceBasePath(), $expected);
+
         $this->assertEquals($expected, $output);
     }
 
+    /**
+     *
+     */
     public function testAbbr()
     {
         $parser = new Parser();
@@ -52,13 +63,34 @@ class InigoRendererTest extends PHPUnit_Framework_TestCase
                 $parser->Parse('[abbr]Traits[/abbr]'));
     }
 
-    public function testFootnotes()
+    /**
+     *
+     */
+    public function testRunSuite()
     {
-        $parser = new Parser();
-        $parser->addHandler(new FootnotesHandler());
+        foreach (glob(__DIR__ . '/examples/inigo/*.txt') as $mdFile) {
+            // Environment
+            $contentToRender = file_get_contents($mdFile);
+            $resourceBasePath = __DIR__ . '/examples/inigo/res';
+            $filter = 'inigo';
 
-        $this->assertEquals(
-                file_get_contents(__DIR__ . '/examples/inigo/footnotes.expected'),
-                $parser->Parse(file_get_contents(__DIR__ . '/examples/inigo/footnotes.txt')));
+            $config = new Config();
+            $config->setResourceBasePath($resourceBasePath);
+
+            $renderer = Factory::createRenderer($config, $filter);
+
+            /* Simulate run of preSave hook */
+            $contentToRender = $renderer->firePreSaveEvent($contentToRender);
+
+            $output   = $renderer->render($contentToRender);
+            $expected = file_get_contents(substr($mdFile, 0, -4) . '.expected');
+
+            $output   = str_replace(array("\r\n", "\r"), "\n", $output);
+            $expected = str_replace(array("\r\n", "\r"), "\n", $expected);
+
+            $expected = str_replace('__RESOURCE_BASE_PATH__', $config->getResourceBasePath(), $expected);
+
+            $this->assertEquals($expected, $output, $mdFile);
+        }
     }
 }
