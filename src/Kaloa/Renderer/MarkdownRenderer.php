4e3ac739f9fa93cddf0c0914522b0dedf7700fa7
Marc Ermshaus <marc@ermshaus.org>
2016-03-12 23:43:11 +0100

    Replace PHP Markdown fork with original version
    PHP Markdown has become available via Packagist in the meantime.

--- src/Kaloa/Renderer/MarkdownRenderer.php
+++ src/Kaloa/Renderer/MarkdownRenderer.php
@@ -3,16 +3,12 @@
 namespace Kaloa\Renderer;
 
 use Kaloa\Renderer\AbstractRenderer;
-use Kaloa\Renderer\Markdown\Hasher;
-use Kaloa\Renderer\Markdown\Parser;
-use Kaloa\Renderer\Markdown\RegexManager;
-use Kaloa\Renderer\Markdown\Encoder;
+use Michelf\Markdown;
 
 class MarkdownRenderer extends AbstractRenderer
 {
     public function render($input)
     {
-        $md = new Parser(new Hasher(), new RegexManager(), new Encoder());
-        return $md->transform($input);
+        return $parser = Markdown::defaultTransform($input);
     }
 }
